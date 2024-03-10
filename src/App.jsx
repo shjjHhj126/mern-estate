f6274376502a460d9189e7132419d7762587c6c3
Sherry <e24089020@gs.ncku.edu.tw>
2024-03-10 22:38:46 +0800

    creates pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { Profiler } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 export default function App() {
-  return <h1 className="text-red">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+//path in <Route>:When the current URL matches the specified path pattern, the associated component specified in the element prop will be rendered.
