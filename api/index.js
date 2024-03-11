6da3f5a17d391a5c7e92d5765e76c235016d1ffd
Sherry <e24089020@gs.ncku.edu.tw>
2024-03-11 14:45:46 +0800

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+//connect to db, mongoose.connect returns a promise
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("connect to db!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
