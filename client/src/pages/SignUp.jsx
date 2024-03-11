541dc55bec8727709cb4ff5753d206b49b6d6bb8
Sherry <e24089020@gs.ncku.edu.tw>
2024-03-11 17:07:10 +0800

    complete signup page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Signout() {
-  return <div>Signup</div>;
+  return (
+    //mx-auto:center the element horizontally within its parent .
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeHolder="username"
+          className="border p-3 rounded-lg"
+          id="username"></input>
+        <input
+          type="text"
+          placeHolder="email"
+          className="border p-3 rounded-lg"
+          id="email"></input>
+        <input
+          type="text"
+          placeHolder="password"
+          className="border p-3 rounded-lg"
+          id="password"></input>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 ">
+          sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in </span>
+        </Link>
+      </div>
+    </div>
+  );
 }
