5ce9cd564b29a8e0e8ee7ec42e9608b8006ec041
Sherry <sherryisamg@gmail.com>
2024-03-18 23:19:40 +0800

    complete listing delete api functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 const Listing = require("../models/listing.model");
+const errorHandler = require("../utils/error");
 
 const createListing = async (req, res, next) => {
   try {
@@ -8,4 +9,20 @@ const createListing = async (req, res, next) => {
     next(err);
   }
 };
-module.exports = createListing;
+const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(404, "You can only delete your own listing"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+module.exports = { createListing, deleteListing };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const verifyToken = require("../utils/verifyUser");
-const createListing = require("../controllers/listing.controller");
+const {
+  createListing,
+  deleteListing,
+} = require("../controllers/listing.controller");
 
 const listingRouter = express.Router();
 
 listingRouter.post("/create", verifyToken, createListing);
+listingRouter.delete("/delete/:id", verifyToken, deleteListing);
 
 module.exports = listingRouter;
