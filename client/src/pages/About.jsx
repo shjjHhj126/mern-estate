2e2288207220f521bc7c61d1594fdec73d1f70be
Sherry <sherryisamg@gmail.com>
2024-03-22 14:15:26 +0800

    complete About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,42 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto ">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sherry Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Sherry Estate is a premier online marketplace dedicated to connecting
+        buyers, sellers, and renters with their ideal properties. With an
+        extensive portfolio of listings spanning residential, commercial, and
+        luxury real estate, Sherry Estate offers a comprehensive platform for
+        individuals and businesses alike to discover their perfect property
+        match.
+      </p>
+      <br />
+      <p className="mb-4 text-slate-700">
+        At Sherry Estate, users can explore a diverse range of properties, from
+        cozy apartments in bustling city centers to sprawling estates nestled in
+        tranquil countryside settings. Our intuitive search and filtering tools
+        empower users to narrow down their options based on their specific
+        preferences, whether it's location, price range, property type, or
+        amenities.
+      </p>
+      <br />
+      <p className="mb-4 text-slate-700">
+        With a user-friendly interface and robust features, Sherry Estate
+        simplifies the property search process, providing detailed property
+        descriptions, high-quality images, and virtual tours to help users make
+        informed decisions. Our dedicated team of real estate professionals is
+        committed to delivering exceptional service and support throughout the
+        buying, selling, or renting journey. Whether you're a first-time buyer,
+        seasoned investor, or property owner looking to list your home, Sherry
+        Estate offers a seamless and efficient platform to fulfill your real
+        estate needs. Experience the convenience and flexibility of online
+        property browsing with Sherry Estate, where your dream home is just a
+        click away.
+      </p>
+    </div>
+  );
 }
