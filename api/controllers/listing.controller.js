58d880dd0f3f5cac6e553f4e11f46c58704ede4c
Sherry <sherryisamg@gmail.com>
2024-04-11 11:28:03 +0800

    fdsfd

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -60,6 +60,7 @@ const getListing = async (req, res, next) => {
   }
 };
 const getListings = async (req, res, next) => {
+  console.log(req);
   try {
     const limit = parseInt(req.query.limit) || 8;
     const startIndex = parseInt(req.query.startIndex) || 0;
