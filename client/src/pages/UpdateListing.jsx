76b90bc78d3dadefd1b827a36cf25a154eb6a4b2
Sherry <sherryisamg@gmail.com>
2024-04-12 22:43:48 +0800

    fix

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -367,7 +367,7 @@ export default function UpdateListing() {
           <button
             disabled={loading || uploading}
             className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            {loading ? "Updating..." : "Update Listing"}
+            {loading ? "Updating..." : "Update"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
